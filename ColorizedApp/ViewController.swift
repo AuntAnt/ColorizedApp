1feeeee892df36740c175ca923925b3bdb813dec
Anton Kuzmin <furthertwoant@gmail.com>
2023-02-07 21:21:27 +0400

    Refactoring

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -20,7 +20,6 @@ final class ViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpSliders()
