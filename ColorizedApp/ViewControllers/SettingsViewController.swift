7ae52a7da1e9bd6848b7b40aba7307510c57d558
Anton Kuzmin <furthertwoant@gmail.com>
2023-02-25 16:49:39 +0400

    Refactored after code review

--- ColorizedApp/ViewControllers/SettingsViewController.swift
+++ ColorizedApp/ViewControllers/SettingsViewController.swift
@@ -87,7 +87,7 @@ final class SettingsViewController: UIViewController {
     }
     
     private func setValue(of slider: UISlider, to label: UILabel, and textField: UITextField) {
-        let value = DataHelper.roundValue(slider.value)
+        let value = roundValue(slider.value)
         label.text = value
         textField.text = value
     }
@@ -114,5 +114,97 @@ final class SettingsViewController: UIViewController {
     
     @objc private func doneToolbarButtonPressed() {
         view.endEditing(true)
+    }
+    
+    /// Normalize text field value if entered value is not in range 0...1
+    private func normalizeValue(number: Float) -> Float {
+        if number > 1 {
+            return Float(1)
+        } else if number < 0 {
+            return Float(0)
+        } else {
+            return number
+        }
+    }
+    
+    private func roundValue(_ value: Float) -> String {
+        String(format: "%.2f", value)
+    }
+}
+
+
+// MARK: - Alert extension
+extension SettingsViewController {
+    func showAlert(_ title: String, _ message: String, _ textField: UITextField) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        
+        let okAction = UIAlertAction(title: "OK", style: .default) {_ in
+            textField.text = "1.00"
+            self.refreshColorState(textField)
+        }
+        
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
+
+// MARK: - Text field delegate extension
+extension SettingsViewController: UITextFieldDelegate{
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        refreshColorState(textField)
+    }
+}
+
+// MARK: - Extension for work with color
+private extension SettingsViewController {
+    func setColorToView() {
+        colorizedView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    func setSlidersColor() {
+        redSlider.minimumTrackTintColor = .systemRed
+        greenSlider.minimumTrackTintColor = .systemGreen
+        blueSlider.minimumTrackTintColor = .systemBlue
+    }
+    
+    func setSlidersValue() {
+        let ciColor = CIColor(color: color)
+        
+        redSlider.value = Float(ciColor.red)
+        greenSlider.value = Float(ciColor.green)
+        blueSlider.value = Float(ciColor.blue)
+    }
+    
+    /// Refreshing slider, slider value labels and colorized view after input new values to text fields
+    func refreshColorState(_ textField: UITextField) {
+        if let textValue = textField.text, let numericValue = Float(textValue) {
+            let value = normalizeValue(number: numericValue)
+            switch textField {
+            case redValueTF:
+                redSlider.setValue(value, animated: true)
+                redSliderValueLabel.text = roundValue(value)
+                textField.text = roundValue(value)
+            case greenValueTF:
+                greenSlider.setValue(value, animated: true)
+                greenSliderValueLabel.text = roundValue(value)
+                textField.text = roundValue(value)
+            default:
+                blueSlider.setValue(value, animated: true)
+                blueSliderValueLabel.text = roundValue(value)
+                textField.text = roundValue(value)
+            }
+            setColorToView()
+        } else {
+            showAlert("Error!", "Wrong number format", textField)
+        }
     }
 }
